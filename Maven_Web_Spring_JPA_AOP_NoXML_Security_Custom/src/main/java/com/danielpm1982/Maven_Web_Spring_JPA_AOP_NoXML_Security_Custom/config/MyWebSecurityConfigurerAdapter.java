a2095496559e0b97a7bf8f80add7406d9b7bde37
Daniel Pinheiro <danielpm1982@gmail.com>
2018-08-13 22:29:46 -0300

    .

---
+++ Maven_Web_Spring_JPA_AOP_NoXML_Security_Custom/src/main/java/com/danielpm1982/Maven_Web_Spring_JPA_AOP_NoXML_Security_Custom/config/MyWebSecurityConfigurerAdapter.java
@@ -0,0 +1,45 @@
+package com.danielpm1982.Maven_Web_Spring_JPA_AOP_NoXML_Security_Custom.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class MyWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		@SuppressWarnings("deprecation")
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		auth.inMemoryAuthentication()
+			.withUser(users.username("user1").password("user1password").roles("ROLE1"))
+			.withUser(users.username("user2").password("user2password").roles("ROLE2"))
+			.withUser(users.username("user3").password("user3password").roles("ROLE3"));
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and()
+		.formLogin().loginPage("/customLoginPage").loginProcessingUrl("/customLoginPageResult")
+		.permitAll();
+	}
+}
+
+/*
+This config class enables and configurates WebSecurity, in this case overriding the
+configure(AuthenticationManagerBuilder auth) method of WebSecurityConfigurerAdapter,
+creating a users builder object with defaultPasswordEncoder (plain-text, not encrypted),
+setting the auth builder with inMemoryAuthentication, and adding each user along with
+their username, password and roles through the users builder object, as arguments. 
+This class also overrides the configure(HttpSecurity http) method to demand that any
+http request be authenticated, as well as to define a custom login page, and permiting all
+users to see the login form.
+The url for the loginPage must match the MySecurityController getMapping url.
+The url for the loginProcessingUrl must match the loginPage action url. 
+The POST request arguments of the loginPage are treated automatically and compared to the 
+previously registered users', for authenticating or not each user, and then for authorizing
+all authenticated users requests for resources thereafter (Session scoped).
+*/
---
+++ Maven_Web_Spring_JPA_AOP_NoXML_Security_Custom/src/main/java/com/danielpm1982/Maven_Web_Spring_JPA_AOP_NoXML_Security_Custom/entity/EntityModelClass.java
@@ -0,0 +1,37 @@
+package com.danielpm1982.Maven_Web_Spring_JPA_AOP_NoXML_Security_Custom.entity;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+public class EntityModelClass {
+	private long id;
+	private LocalDateTime dateTime;
+	private String dateTimeStringified;
+	private String entityDescription;
+	public EntityModelClass() {
+		dateTime = LocalDateTime.now();
+		dateTimeStringified = dateTime.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.FULL, FormatStyle.MEDIUM));
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+	public String getDateTimeStringified() {
+		return dateTimeStringified;
+	}
+	public String getEntityDescription() {
+		return entityDescription;
+	}
+	public void setEntityDescription(String entityDescription) {
+		this.entityDescription = entityDescription;
+	}
+	@Override
+	public String toString() {
+		return "Id: "+id+" Description: "+entityDescription+" DateTime: "+dateTimeStringified;
+	}
+}
