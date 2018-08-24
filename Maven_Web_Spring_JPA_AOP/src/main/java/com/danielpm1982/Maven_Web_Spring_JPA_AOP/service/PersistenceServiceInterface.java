4580f7a1ed534a4ea3131dbdb9fe1112cb72a8bf
Daniel Pinheiro <danielpm1982@gmail.com>
2018-08-23 22:32:49 -0300

    .

--- Maven_Web_Spring_JPA_AOP/src/main/java/com/danielpm1982/Maven_Web_Spring_JPA_AOP/service/PersistenceServiceInterface.java
+++
@@ -1,8 +0,0 @@
-package com.danielpm1982.Maven_Web_Spring_JPA_AOP.service;
-import java.util.List;
-import com.danielpm1982.Maven_Web_Spring_JPA_AOP.entity.EntityModelClass;
-
-public interface PersistenceServiceInterface{
-	public List<EntityModelClass> searchAllEntities();
-	public boolean insertEntity(EntityModelClass entityModelClass);
-}
