ffb4eacd0745c28d94f3335798c91c397faee21a
Daniel Pinheiro <danielpm1982@gmail.com>
2018-11-16 17:32:00 -0300

    .

--- Maven_Web_Spring_JPA_AOP_NoXML_Security_Custom_AuthDB_AuthDataCrud/src/main/java/com/danielpm1982/Maven_Web_Spring_JPA_AOP_NoXML_Security_Custom_AuthDB_AuthDataCrud/dao/UserDaoInterface.java
+++
@@ -1,9 +0,0 @@
-package com.danielpm1982.Maven_Web_Spring_JPA_AOP_NoXML_Security_Custom_AuthDB_AuthDataCrud.dao;
-import com.danielpm1982.Maven_Web_Spring_JPA_AOP_NoXML_Security_Custom_AuthDB_AuthDataCrud.entity.User;
-
-public interface UserDaoInterface {
-    public abstract User findByUserName(String userName);
-    public abstract void save(User user);
-    public abstract User findByUserId(Long userId);
-    public abstract void deleteUserById(Long userId);
-}
