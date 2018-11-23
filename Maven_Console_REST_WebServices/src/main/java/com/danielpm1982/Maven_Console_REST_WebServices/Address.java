cb416bc60b2bb38a4c6f1d15891e8409b3e9ea6e
Daniel Pinheiro <danielpm1982@gmail.com>
2018-11-23 20:35:19 -0300

    .

---
+++ Maven_Console_REST_WebServices/src/main/java/com/danielpm1982/Maven_Console_REST_WebServices/Address.java
@@ -0,0 +1,45 @@
+package com.danielpm1982.Maven_Console_REST_WebServices;
+
+public class Address {
+	private String street;
+	private String number;
+	private String city;
+	private String state;
+	private String country;
+	public Address() {
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	@Override
+	public String toString() {
+		return "street: "+street+" number: "+number+" city: "+city+" state: "+state+" country: "+country;
+	}
+}
