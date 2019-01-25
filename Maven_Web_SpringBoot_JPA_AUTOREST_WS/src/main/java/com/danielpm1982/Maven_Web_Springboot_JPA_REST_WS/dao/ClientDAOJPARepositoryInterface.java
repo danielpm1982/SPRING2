50c5f3a468b8faa261f3d796908b26be00f1708a
Daniel Pinheiro <danielpm1982@gmail.com>
2019-01-25 06:54:50 -0300

    .

--- Maven_Web_SpringBoot_JPA_AUTOREST_WS/src/main/java/com/danielpm1982/Maven_Web_Springboot_JPA_REST_WS/dao/ClientDAOJPARepositoryInterface.java
+++
@@ -1,6 +0,0 @@
-package com.danielpm1982.Maven_Web_Springboot_JPA_REST_WS.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.danielpm1982.Maven_Web_Springboot_JPA_REST_WS.entity.Client;
-
-public interface ClientDAOJPARepositoryInterface extends JpaRepository<Client, Long> {
-}
