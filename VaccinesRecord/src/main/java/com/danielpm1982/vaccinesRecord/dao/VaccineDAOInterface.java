48ba15ccf66b5c93d34603e8e9c71ce71b0a8639
Daniel Pinheiro <danielpm1982@gmail.com>
2019-03-15 21:15:05 -0300

    .

--- VaccinesRecord/src/main/java/com/danielpm1982/vaccinesRecord/dao/VaccineDAOInterface.java
+++
@@ -1,9 +0,0 @@
-package com.danielpm1982.vaccinesRecord.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.danielpm1982.vaccinesRecord.entity.Vaccine;
-
-@Repository
-public interface VaccineDAOInterface extends JpaRepository<Vaccine, Long> {
-
-}
--- VaccinesRecord/src/main/java/com/danielpm1982/vaccinesRecord/service/PatientServiceInterface.java
+++
@@ -1,7 +0,0 @@
-package com.danielpm1982.vaccinesRecord.service;
-import com.danielpm1982.vaccinesRecord.entity.Patient;
-
-public interface PatientServiceInterface {
-	public Patient getPatientByPatientId(long patientId);
-	public Patient addNewPatient(Patient patient);
-}
