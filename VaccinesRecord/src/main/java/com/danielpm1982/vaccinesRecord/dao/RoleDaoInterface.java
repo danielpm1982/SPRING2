7b4481765d80feaec1e7e52da249f6223871d6a4
Daniel Pinheiro <danielpm1982@gmail.com>
2019-03-16 09:56:03 -0300

    .

--- VaccinesRecord/src/main/java/com/danielpm1982/vaccinesRecord/dao/RoleDaoInterface.java
+++
@@ -1,6 +0,0 @@
-package com.danielpm1982.vaccinesRecord.dao;
-import com.danielpm1982.vaccinesRecord.entity.Role;
-
-public interface RoleDaoInterface {
-	public abstract Role findRoleByName(String roleName);
-}
